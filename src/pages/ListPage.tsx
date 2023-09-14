e2bc68f5f342d6a77c773c4f5f05df0eb6691d69
Jonathan DeKlotz <jdeklotz@live.com>
2023-09-14 14:50:23 -0700

    Add filtering controls

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react"
+import React, { useState } from "react"
 import { useResizeDetector } from "react-resize-detector"
 import { FixedSizeList } from 'react-window'
 import { allGlyphs, preprocess } from "../data"
@@ -6,14 +6,26 @@ import { GlyphCard, PenChar } from "../PenChar"
 
 import "./ListPage.scss"
 
+const character_classes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '=', '<', '>'];
+
 const glyphs = allGlyphs.map(preprocess);
 
-export const GlyphList: React.FC = () => {
+export interface GlyphListProps {
+  shouldFilterByClass: boolean;
+  selectedGlyphClass: string;
+}
+
+export const GlyphList: React.FC<GlyphListProps> = ({ shouldFilterByClass, selectedGlyphClass }) => {
+
+  const renderedGlyphs =
+    shouldFilterByClass ?
+      glyphs.filter(g => g.character === selectedGlyphClass) :
+      glyphs;
 
   const renderRow = ({index, style}: {index: number, style: React.CSSProperties}) => {
     return (
       <div style={style}>
-        <GlyphCard glyph={glyphs[index]} />
+        <GlyphCard glyph={renderedGlyphs[index]} />
       </div>
     )
   }
@@ -23,10 +35,10 @@ export const GlyphList: React.FC = () => {
   return (
     <div className="list" ref={ref}>
       <FixedSizeList
-        height={height ?? 0}
+        height={height ? height : 0}
         width={width ?? 0}
         itemSize={200}
-        itemCount={glyphs.length}
+        itemCount={renderedGlyphs.length}
         overscanCount={5}
       >
         {renderRow}
@@ -36,9 +48,37 @@ export const GlyphList: React.FC = () => {
 }
 
 export const ListPage: React.FC = () => {
+  const [shouldFilterByClass, setShouldFilterByClass] = useState(false);
+  const [selectedGlyphClass, setSelectedGlyphClass] = useState('0');
+  
   return (
     <div className="list-page">
-      <GlyphList />
+      <div className="filter-controls">
+        <label>
+          <input
+            type="checkbox"
+            name="showCheckbox"
+            id="showCheckbox"
+            checked={shouldFilterByClass}
+            onChange={e => setShouldFilterByClass(e.target.checked)}
+          />
+          Filter examples by class: 
+        </label>
+        <select
+          className="class-dropdown"
+          value={selectedGlyphClass}
+          onChange={e => setSelectedGlyphClass(e.target.value)}>
+            {
+              character_classes.map(c =>
+                <option value={c}>{c}</option>
+              )
+            }
+        </select>
+      </div>
+
+      <GlyphList
+        shouldFilterByClass={shouldFilterByClass}
+        selectedGlyphClass={selectedGlyphClass} />
     </div>
   );
-}
+};
